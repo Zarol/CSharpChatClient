3a51570d7d5374b96e8f6b660a7a16254f853db4
Saharath Kleips <saharathkleips@nevada.unr.edu>
2015-04-25 00:31:30 -0700

    Initial Commit
    Some initial commits with a working TCP Server. The entirety of the code
    has yet to be fleshed out.

---
+++ CSharpChatClient/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharpChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        TcpClient server;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Connect the client to the server (localhost).
+         */
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            string ipAddress = "192.168.1.120";
+            server = new TcpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), 3000);
+
+            try
+            {
+                server.Connect(serverEndPoint);
+
+                Thread serverThread = new Thread(new ParameterizedThreadStart(HandleServerComm));
+                serverThread.Start(server);
+
+                btnConnect.IsEnabled = false;
+            }
+            catch
+            {
+                System.Diagnostics.Debug.WriteLine("[Client] Could not connect to server: " + ipAddress);
+            }
+        }
+
+        /**
+         * Sends the server a message within the textbox.
+         */
+        private void btnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            //NetworkStream clientStream = server.GetStream();
+
+            //ASCIIEncoding encoder = new ASCIIEncoding();
+            //byte[] buffer = encoder.GetBytes(txtBoxMessage.Text);
+
+            //clientStream.Write(buffer, 0, buffer.Length);
+            //clientStream.Flush();
+            ChatLog = "Hey, how's it going?/nPretty good, how about yourself?";
+            txtBoxMessage.Text = ChatLog;
+        }
+
+        /**
+         * Acquire NetworkStream from the TcpClient to read.
+         * Loop reading all the information from the server.
+         * If no bytes have been recieved, the server has been disconnected.
+         */
+        private void HandleServerComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("[Client] A socket error has occured.");
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("[Client] Server has been disconnected.");
+                    break;
+                }
+
+                //Message has been successfully recieved
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                //broadcastMessage(encoder.GetString(message, 0, bytesRead));
+            }
+
+            tcpClient.Close();
+        }
+
+        private string _chatLog;
+        public string ChatLog
+        {
+            get { return _chatLog; }
+            set
+            {
+                _chatLog = value;
+                OnPropertyChanged("ChatLog");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ThreadedTCPServer/Server.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Collections;
+
+namespace ThreadedTCPServer
+{
+    class Server
+    {
+        private TcpListener tcpListerner;
+        private Thread listenThread;
+
+        private ArrayList userList;
+
+        public Server()
+        {
+            this.userList = new ArrayList();
+            this.tcpListerner = new TcpListener(IPAddress.Any, 3000);
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+        }
+
+        /**
+         * Starts the TcpListener, then sits in a loop accepting connections.
+         * AcceptTcpClient will block until a client has connected, which will then
+         * use a thread to handle communication with the new client.
+         */
+        private void ListenForClients()
+        {
+            this.tcpListerner.Start();
+
+            while (true)
+            {
+                //Blocks until a client has connected to the server
+                TcpClient tcpClient = this.tcpListerner.AcceptTcpClient();
+
+                //Store the connected client into an ArrayList
+                userList.Add(tcpClient);
+
+                //Create a thread to handle communication with connected client
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(tcpClient);
+            }
+        }
+
+        /**
+         * Acquire NetworkStream from the TcpClient to read.
+         * Loop reading all the information from the client.
+         * If no bytes have been recieved, the client has disconnected.
+         */
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("[Server] A socket error has occured.");
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("[Server] Client has disconnected.");
+                    break;
+                }
+
+                //Message has been successfully recieved
+                //ASCIIEncoding encoder = new ASCIIEncoding();
+                //System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                broadcastMessage(encoder.GetString(message, 0, bytesRead));
+            }
+
+            tcpClient.Close();
+        }
+
+        /**
+         * Broadcasts a string to all currently connected clients.
+         */
+        private void broadcastMessage(string message)
+        {
+            NetworkStream clientStream;
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes(message);
+
+            //Locking the list guarantees the thread safety of the ArrayList
+            lock (userList.SyncRoot)
+            {
+                foreach (TcpClient user in userList)
+                {
+                    clientStream = user.GetStream();
+                    clientStream.Write(buffer, 0, buffer.Length);
+                    clientStream.Flush();
+                }
+            }
+        }
+    }
+}
